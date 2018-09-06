984378fef50a9401b444d87bc85d7c4823d96ce3
aldemarmendes <aldemarazevedo2015@hotmail.com>
2018-09-06 20:59:43 -0300

    Update test_calculadora.py

--- test_calculadora.py
+++ test_calculadora.py
@@ -4,10 +4,10 @@ from result import soma, sub, mult, div
 
 def test_calculadora:
     
-    assert soma(6) + 7 == 13
-    assert sub (6) - 6 == 0
-    assert mult(7) * 2 == 14
-    assert div (4) / 2 == 2
+    assert soma(6,7) == 13
+    assert sub (6,6) == 0
+    assert mult(7,2) == 14
+    assert div (4,2) == 2
     
     
     
