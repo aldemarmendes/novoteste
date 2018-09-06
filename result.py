64a2a02565e3f1cc3fea39b39ea558a2c192a9d1
aldemarmendes <aldemarazevedo2015@hotmail.com>
2018-09-06 20:58:36 -0300

    Update result.py

--- result.py
+++ result.py
@@ -1,9 +1,4 @@
-def numero1():
-numero1 = float(imput("INFORME O NUMERO  1 ?"))
-
-def numero2():
-numero2 = float(imput)(" INFORME O NUMERO 2 ?"))
-
-def operacao():
-    operacao = imput("Informe a Operação ?")
+def soma(num1,num2):
+    try:
+        
     
