ed2a5f17e9f85dfc407ba425e004edbf60772270
aldemarmendes <aldemarazevedo2015@hotmail.com>
2018-09-06 21:03:31 -0300

    resultado do teste

--- result.py
+++ result.py
@@ -1,4 +1,20 @@
 def soma(num1,num2):
     try:
+        return float(num1) + float(num2)
+               except:
+            return None
+def sub(num1,num2):
+    try:
+        return float(num1) - float(num2)
+               except:
+            return None
+def mult(num1, num2):
+    try:
+        return float(num1) * float (num2)
+    return None
+def div(num1, num2):
+    try:
+        return float(num1) * float(num2)
+    return None
         
     
